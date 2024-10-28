32052ca1339fbcdb5440f749de9e16758cb82863
Magomed <me@sekkeio.me>
2024-10-28 06:06:46 +0300

    Fixed script

--- assets/js/langRedirect.js
+++ assets/js/langRedirect.js
@@ -1,6 +1,15 @@
-const userLang = navigator.language || navigator.userLanguage;
-if (userLang.startsWith('ru')) {
-    window.location.href = '/';
-} else {
-    window.location.href = '/en';
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const userLang = navigator.language || navigator.userLanguage; 
+    const redirected = localStorage.getItem('redirected');
+    
+    if (!redirected) {
+        localStorage.setItem('redirected', 'true');
+        setTimeout(() => {
+            if (userLang.startsWith('ru')) {
+                window.location.href = '/'; 
+            } else {
+                window.location.href = '/en'; 
+            }
+        }, 300);
+    }
+});
