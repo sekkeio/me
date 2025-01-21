20010a4b6b72846e01ae5714aa1c56b89cb7c1a4
Magomed <me@sekkeio.me>
2025-01-21 12:21:07 +0300

    feat: add auto-save

---
+++ assets/js/board.js
@@ -0,0 +1,13 @@
+const elements = {
+    textarea: document.querySelector('.board_area'),
+    input: document.querySelector('.topic_input')
+};
+
+window.onload = () => {
+    for (const key in elements) {
+        elements[key].value = localStorage.getItem(key + 'Value') || '';
+        elements[key].addEventListener('input', () => {
+            localStorage.setItem(key + 'Value', elements[key].value);
+        });
+    }
+};
