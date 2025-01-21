113e78fdccd97187877ff76deaa3e2b0416a9033
Magomed <me@sekkeio.me>
2025-01-21 23:34:16 +0300

    feat: logic links touch

--- assets/js/bureau.js
+++ assets/js/bureau.js
@@ -8,3 +8,19 @@ const surroundedByQuotationMarks = new RegExp(`^([${QUOTATION_MARKS}]{1})(.*)([$
 Array.from(document.querySelectorAll('a'))
   .filter(a => surroundedByQuotationMarks.test(a.textContent))
   .forEach(a => a.innerHTML = a.innerHTML.replace(surroundedByQuotationMarks, '$1<u>$2</u>$3'))
+
+// Логика анимации ссылок на тач-экранах
+if ('ontouchstart' in window || navigator.maxTouchPoints) {
+    const links = document.querySelectorAll('a');
+
+    links.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const url = this.href;
+            this.classList.add('hover');
+            setTimeout(() => {
+                window.location.href = url;
+            }, 200);
+        });
+    });
+}
